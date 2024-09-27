a9d99dbfa0bd2e5403635e2c5d74a83eb4355b8b
mahum98 <mahum985@gmail.com>
2024-09-27 22:22:42 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(routes: {"/": (context) => WidgetTree()});
+    return MaterialApp(
+      debugShowCheckedBanner: false;
+      routes: {"/": (context) => WidgetTree()});
   }
 }
