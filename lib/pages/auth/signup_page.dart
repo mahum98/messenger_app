ac66b8b6c2005a702aa9da5f2f85144d6ecda2d8
mahum98 <mahum985@gmail.com>
2024-09-28 00:03:08 +0500

    Update signup_page.dart

--- lib/pages/auth/signup_page.dart
+++ lib/pages/auth/signup_page.dart
@@ -26,19 +26,40 @@ class SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
+      backgroundColor: Theme.of(context).colorScheme.background,
       body: SafeArea(
+      child: SingleChildScrollView(
           child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical:150),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.person, size: 200),
+            Icon(Icons.person_add, size: 100,
+              color: Theme.of(context).colorScheme.primary,),
+            SizedBox(height: 30),
+
+            Text(
+            "Create Account",
+            style: TextStyle(
+            fontSize: 16,
+
+            color: Theme.of(context).colorScheme.onBackground,
+            ),
+            ),
             SizedBox(height: 20),
+
+            //email input
             Container(
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  color: Theme.of(context).colorScheme.primary),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 8,
+                      spreadRadius: 2,
+                    ),
+                  ],
+              ),
               child: TextField(
                 onTapOutside: (event) {
                   FocusScope.of(context).unfocus();
@@ -46,95 +67,121 @@ class SignupPageState extends State<SignupPage> {
                 controller: emailController,
                 decoration: InputDecoration(
                     hintText: "example@gmail.com",
+                    prefixIcon: Icon(Icons.email, color: Colors.grey),
+                    filled: true,
+                    fillColor: Theme.of(context).colorScheme.surface,
                     enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.primary),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
                     focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.primary),
-                        borderRadius: BorderRadius.all(Radius.circular(10)))),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.primary),
+                    ),
+                ),
               ),
             ),
             SizedBox(height: 5),
+
+            //password
             Container(
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  color: Theme.of(context).colorScheme.primary),
-              child: TextField(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 8,
+                    spreadRadius: 2,
+                  ),
+                ],
+
+              ),child: TextField(
                 onTapOutside: (event) {
                   FocusScope.of(context).unfocus();
                 },
                 controller: pswdController,
+    obscureText: true,
                 decoration: InputDecoration(
-                    hintText: "password",
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.primary),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.primary),
-                        borderRadius: BorderRadius.all(Radius.circular(10)))),
-              ),
-            ),
+    hintText: "Password",
+    prefixIcon: Icon(Icons.lock, color: Colors.grey),
+    filled: true,
+    fillColor: Theme.of(context).colorScheme.surface,
+    enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide(color: Colors.grey[300]!),
+    ),
+    focusedBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide(
+    color: Theme.of(context).colorScheme.primary),
+    ),
+    ),
+    ),
+    ),
             SizedBox(height: 10),
-            Text("$errorMessage", style: TextStyle(color: Colors.red[700])),
-            SizedBox(height: 10),
-            TextButton(
-                onPressed: (emailController.text.isNotEmpty &&
-                        pswdController.text.isNotEmpty)
-                    ? () async {
-                        print("in system request");
-                        try {
-                          await auth.createUser(
-                              email: emailController.text,
-                              password: pswdController.text);
-                          //extra functionality (add user email to UserCollection)
-                          UsersCollection usersCollection = UsersCollection();
-                          String userName = emailController.text;
-                          int index = userName.indexOf("@");
-                          userName = userName.substring(0, index);
-                          usersCollection.addUserEmail(userEmail: userName);
-                          //end of extra functionality
-                        } on FirebaseAuthException catch (e) {
-                          setState(() {
-                            errorMessage = e.message;
-                          });
-                        }
-                      }
-                    : null,
-                style: ButtonStyle(
-                    shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)))),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Sign In"),
-                    ],
-                  ),
-                )),
+    // Error Message Display
+    if (errorMessage != null && errorMessage!.isNotEmpty)
+    Text(
+    "$errorMessage",
+    style: TextStyle(color: Colors.red, fontSize: 14),
+    ),
+    SizedBox(height: 15),
+    //signup button
+    ElevatedButton(
+    onPressed: (emailController.text.isNotEmpty &&
+    pswdController.text.isNotEmpty)
+    ? () async {
+    print("in system request");
+    try {
+    await auth.createUser(
+    email: emailController.text,
+    password: pswdController.text,
+    );
+    // Extra functionality (add user email to UserCollection)
+    UsersCollection usersCollection = UsersCollection();
+    String userName = emailController.text;
+    int index = userName.indexOf("@");
+    userName = userName.substring(0, index);
+    usersCollection.addUserEmail(userEmail: userName);
+    } on FirebaseAuthException catch (e) {
+    setState(() {
+    errorMessage = e.message;
+    });
+    }
+    }
+        : null,
+    style: ElevatedButton.styleFrom(
+    backgroundColor: Theme.of(context).colorScheme.primary,
+    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 80),
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(10),
+    ),
+    ),child: Text(
+    "Sign Up",
+    style: TextStyle(fontSize: 18),
+    ),
+    ),
             SizedBox(height: 30),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text("Already have an account? ",
                     style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary)),
+                        color: Theme.of(context).colorScheme.onBackground)),
                 GestureDetector(
                     onTap: () {
                       flag = true;
                       refreshParent();
                     },
                     child: Text("LogIn",
-                        style: TextStyle(fontWeight: FontWeight.bold)))
+                        style: TextStyle(fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary)))
               ],
             )
           ],
         ),
       )),
-    );
+    ));
   }
 }
